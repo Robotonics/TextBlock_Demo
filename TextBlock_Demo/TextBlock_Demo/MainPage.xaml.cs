16d64a7f55dcd77973b9709a1487986ddf2d956d
robotonics <roboshopz@gmail.com>
2016-02-10 18:18:18 +0000

    Initial Upload

---
+++ TextBlock_Demo/TextBlock_Demo/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace TextBlock_Demo
+
+{
+   
+    public sealed partial class MainPage : Page
+    {
+        Student student = new Student();
+
+        public void constructStudent()
+        {
+            student.Name = "David";
+            student.Rollno = "ENG-001";
+            student.Phone = "01159989838";
+        }
+
+        public MainPage()
+
+        {
+            this.InitializeComponent();
+            constructStudent();
+            this.DataContext = student;
+
+        }
+    }
+}
---
+++ TextBlock_Demo/TextBlock_Demo/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBlock_Demo
+{
+    class Student
+    {
+        private string name_;
+        public string Name
+        {
+            get { return name_; }
+            set { name_ = value; }
+        }
+        private string rollno_;
+        public string Rollno
+        {
+            get { return rollno_; }
+            set { rollno_ = value; }
+        }
+        private string phone_;
+        public string Phone
+        {
+            get { return phone_; }
+            set { phone_ = value; }
+        }
+
+    }
+}
